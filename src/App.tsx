a02e34c3f4ca1d1074ff28ca10058c66363c2701
Ivan <ivanyu327@gmail.com>
2023-05-29 02:15:44 -0400

    crazy tetris animation bro

--- src/App.tsx
+++ src/App.tsx
@@ -14,43 +14,73 @@ const AppWrapper = styled.div`
   position: relative;
 `;
 
-const LoadingContent = styled.div<{ fadeOut: boolean }>`
-  opacity: 1;
-  transition: opacity 0.5s;
-
-  ${({ fadeOut }) =>
-    fadeOut &&
-    `
-    animation: ${fadeOut} 0.5s forwards;
-  `}
+const TetrisPiece = styled.div<{ expanded: boolean }>`
+  width: 10px;
+  height: 40px;
+  position: absolute;
+  top: calc(50vh - 20px);
+  left: calc(50vw + 10px);
+  background-color: black;
+  animation: ${tetrisAnimation} 1s forwards;
+  transform: translate(-50%, -50%)
+    scale(${({ expanded }) => (expanded ? 1.4 : 1)});
+  opacity: ${({ expanded }) => (expanded ? 0 : 1)};
+  transition: all 1s ease-out;
+  background-color: ${({ expanded }) => (expanded ? "white" : "black")};
 `;
 
-const WebsiteContent = styled.div<{ fadeIn: boolean }>`
-  opacity: 0;
-  transition: opacity 0.5s;
+const TetrisContainer = styled.div<{ expanded: boolean }>`
+  width: 100px;
+  height: 40px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -50%)
+    scale(${({ expanded }) => (expanded ? 1.4 : 1)});
+  opacity: ${({ expanded }) => (expanded ? 0 : 1)};
+  transition: all 1s ease-out;
+  background-color: ${({ expanded }) => (expanded ? "black" : "")};
+`;
 
-  ${({ fadeIn }) =>
-    fadeIn &&
-    `
-    animation: ${fadeIn} 0.5s forwards;
-  `}
+const Rectangle = styled.div<{
+  expanded: boolean;
+  width: number;
+  height: number;
+  color: string;
+}>`
+  background-color: ${({ expanded, color }) => (expanded ? "black" : color)};
+  width: ${({ width }) => 10 * width}px;
+  height: ${({ height }) => 10 * height}px;
+`;
+
+const WebsiteContent = styled.div<{ expanded: boolean }>`
+  opacity: ${({ expanded }) => (expanded ? 1 : 0)};
+  transition: opacity 0.5s;
 `;
 
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(true);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false);
-    }, 3000);
+      setExpanded(true);
+    }, 2000);
   }, []);
 
   return (
     <AppWrapper>
-      <LoadingContent fadeOut={!loading}>
-        {loading && "Loading Animation"}
-      </LoadingContent>
-      <WebsiteContent fadeIn={!loading}>Actual Website Content</WebsiteContent>
+      <TetrisContainer expanded={expanded}>
+        <Rectangle expanded={expanded} height={4} width={6} color="black" />
+        <Rectangle expanded={expanded} height={4} width={1} color="white" />
+        <Rectangle expanded={expanded} height={4} width={3} color="black" />
+      </TetrisContainer>
+      <TetrisPiece expanded={expanded} />
+      <WebsiteContent expanded={expanded}>
+        Actual Website Content
+      </WebsiteContent>
     </AppWrapper>
   );
 };
