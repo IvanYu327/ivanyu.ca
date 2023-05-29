3132b93a78c5e248931a60f40d0516402422060d
Ivan <ivanyu327@gmail.com>
2023-05-29 02:26:57 -0400

    refactor and organize

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,20 @@
 import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 
-const tetrisAnimation = keyframes`
-  0% {
-    transform: translateY(-60vh);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
-
-const AppWrapper = styled.div`
-  position: relative;
-`;
-
-const TetrisPiece = styled.div<{ expanded: boolean }>`
-  width: 10px;
-  height: 40px;
-  position: absolute;
-  top: calc(50vh - 20px);
-  left: calc(50vw + 10px);
-  background-color: black;
-  animation: ${tetrisAnimation} 1s forwards;
-  transform: translate(-50%, -50%)
-    scale(${({ expanded }) => (expanded ? 1.4 : 1)});
-  opacity: ${({ expanded }) => (expanded ? 0 : 1)};
-  transition: all 1s ease-out;
-  background-color: ${({ expanded }) => (expanded ? "white" : "black")};
-`;
-
-const TetrisContainer = styled.div<{ expanded: boolean }>`
-  width: 100px;
-  height: 40px;
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 50vh;
-  left: 50vw;
-  transform: translate(-50%, -50%)
-    scale(${({ expanded }) => (expanded ? 1.4 : 1)});
-  opacity: ${({ expanded }) => (expanded ? 0 : 1)};
-  transition: all 1s ease-out;
-  background-color: ${({ expanded }) => (expanded ? "black" : "")};
-`;
-
-const Rectangle = styled.div<{
-  expanded: boolean;
-  width: number;
-  height: number;
-  color: string;
-}>`
-  background-color: ${({ expanded, color }) => (expanded ? "black" : color)};
-  width: ${({ width }) => 10 * width}px;
-  height: ${({ height }) => 10 * height}px;
-`;
-
-const WebsiteContent = styled.div<{ expanded: boolean }>`
-  opacity: ${({ expanded }) => (expanded ? 1 : 0)};
-  transition: opacity 0.5s;
-`;
+const TETRIS_UNIT_SQUARE = 10;
+const ANIMATION_TIME = 1500;
 
 const App: React.FC = () => {
   const [expanded, setExpanded] = useState(false);
+  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       setExpanded(true);
-    }, 2000);
+    }, ANIMATION_TIME);
+    setTimeout(() => {
+      setShowContent(true);
+    }, ANIMATION_TIME + 500);
   }, []);
 
   return (
@@ -78,7 +25,7 @@ const App: React.FC = () => {
         <Rectangle expanded={expanded} height={4} width={3} color="black" />
       </TetrisContainer>
       <TetrisPiece expanded={expanded} />
-      <WebsiteContent expanded={expanded}>
+      <WebsiteContent showContent={showContent}>
         Actual Website Content
       </WebsiteContent>
     </AppWrapper>
@@ -86,3 +33,62 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+const AppWrapper = styled.div`
+  position: relative;
+`;
+
+const tetrisAnimation = keyframes`
+  0% {
+    transform: translateY(-60vh);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+const TetrisPiece = styled.div<{ expanded: boolean }>`
+  width: ${TETRIS_UNIT_SQUARE}px;
+  height: ${TETRIS_UNIT_SQUARE * 4}px;
+  position: absolute;
+  top: calc(50vh - ${TETRIS_UNIT_SQUARE * 2}px);
+  left: calc(50vw + ${TETRIS_UNIT_SQUARE}px);
+  background-color: black;
+  animation: ${tetrisAnimation} ${ANIMATION_TIME * 0.5}ms forwards;
+  transform: translate(-50%, -50%)
+  opacity: ${({ expanded }) => (expanded ? 0 : 1)};
+  transition: all ${ANIMATION_TIME}ms ease-out;
+  display: ${({ expanded }) => (expanded ? "none" : "")};
+`;
+
+const TetrisContainer = styled.div<{ expanded: boolean }>`
+  width: ${TETRIS_UNIT_SQUARE * 10}px;
+  height: ${TETRIS_UNIT_SQUARE * 4}px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -50%)
+    scale(${({ expanded }) => (expanded ? 1.4 : 1)});
+  opacity: ${({ expanded }) => (expanded ? 0 : 1)};
+  transition: all ${ANIMATION_TIME}ms ease-out;
+  background-color: ${({ expanded }) => (expanded ? "black" : "")};
+`;
+
+const Rectangle = styled.div<{
+  expanded: boolean;
+  width: number;
+  height: number;
+  color: string;
+}>`
+  background-color: ${({ expanded, color }) => (expanded ? "black" : color)};
+  width: ${({ width }) => TETRIS_UNIT_SQUARE * width}px;
+  height: ${({ height }) => TETRIS_UNIT_SQUARE * height}px;
+`;
+
+const WebsiteContent = styled.div<{ showContent: boolean }>`
+  opacity: ${({ showContent }) => (showContent ? 1 : 0)};
+  transition: opacity 0.5s;
+`;
