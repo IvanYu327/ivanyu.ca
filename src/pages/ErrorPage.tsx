84b72158fbab7b7f8555248dc8a0d5cfd4beaf58
Ivan <ivanyu327@gmail.com>
2023-06-07 19:22:54 -0400

    fix error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
 
 import styled from "styled-components";
 
-// import { Container } from "../components/globalComponents";
-
 const PageNotFoundMessage = styled.h3`
-  margin-top: 30vh;
-  margin-bottom: 50vh;
   text-align: center;
 `;
 
-const ErrorPage = () => {
-  // useRouteError((error, info) => {
-  //   // Handle the route error here
-  //   console.log("Route error:", error, info);
-  // });
-
+const ErrorPage: React.FC = () => {
   return (
     <div>
       <PageNotFoundMessage>
