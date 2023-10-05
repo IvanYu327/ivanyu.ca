51f35f2ac782a18b46f1f2d59f86a963d6389970
Ivan <ivanyu327@gmail.com>
2023-10-05 16:21:31 -0400

    nav and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Box>Made with Ivan</Box>
+      <SocialsContainer>
+        <SocialButton>L</SocialButton>
+        <SocialButton>G</SocialButton>
+        <SocialButton>X</SocialButton>
+      </SocialsContainer>
+    </Container>
+  );
+};
+
+export default Footer;
+
+const Container = styled.div`
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  border-top: 0.5px solid white;
+  margin-top: 32px;
+  padding-top: 16px;
+  background: red;
+`;
+
+const Box = styled.div``;
+
+const SocialsContainer = styled.div`
+  display: flex;
+`;
+
+const SocialButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: green;
+  height: 24px;
+  width: 24px;
+  margin-left: 8px;
+`;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar: React.FC = () => {
+  return <Container>Navbar</Container>;
 };
 
 export default Navbar;
+
+const Container = styled.div`
+  height: 100px;
+  background: red;
+  border-bottom: 0.5px solid white;
+  margin-bottom: 32px;
+`;
