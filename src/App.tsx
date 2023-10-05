6d563364a24b405ba16d00fd9963c58a571aadea
Ivan <ivanyu327@gmail.com>
2023-10-05 11:35:05 -0400

    all the homies hate repo setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-// import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { ROUTES } from "./constants/routes";
+import Home from "./views/Home";
 
 function App() {
-  return <div className="App">balls</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.MAIN} element={<Home />} />
+        <Route path={ROUTES.ABOUT} element={<div>about</div>} />
+
+        <Route path="*" element={<div>not found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
