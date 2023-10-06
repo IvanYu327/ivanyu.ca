f9a1818642cb2dfbc9211740bdec87b7e938bc15
Ivan <ivanyu327@gmail.com>
2023-10-06 16:08:52 -0400

    types

---
+++ src/copy/experience.ts
@@ -0,0 +1,48 @@
+import { Experience } from "../typings/types";
+
+export const EXPERIENCES: Experience[] = [
+  {
+    position: "Backend Developer",
+    company: "Hack the North",
+    startYear: 2023,
+    endYear: null,
+    link: ""
+  },
+  {
+    position: "Software Engineering Intern",
+    company: "Deep Breathe",
+    startYear: 2023,
+    endYear: null,
+    link: ""
+  },
+  {
+    position: "Software Team Lead",
+    company: "WatoLink",
+    startYear: 2021,
+    endYear: 2023,
+    link: ""
+  },
+  {
+    position: "Frontend Developer",
+    company: "Hack the North",
+    startYear: 2023,
+    endYear: 2023,
+    link: ""
+  },
+  {
+    position: "Software Engineering Intern",
+    company: "iVenuto.com",
+    startYear: 2023,
+    endYear: 2023,
+    link: ""
+  },
+  {
+    position: "Software Developer Intern",
+    company: "Cognixion",
+    startYear: 2022,
+    endYear: 2022,
+    link: ""
+  }
+];
+
+export default EXPERIENCES;
