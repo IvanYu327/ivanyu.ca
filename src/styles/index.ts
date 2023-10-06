f8a204595fdd71e9ec942859dfe4d1de5bec3c82
Ivan <ivanyu327@gmail.com>
2023-10-05 23:39:15 -0400

    styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -48,85 +48,96 @@ export const theme: DefaultTheme = {
 
 export const Heading1 = styled.h1`
   margin: 0px;
-  font-family: "Castledown";
+  font-family: "Inter";
   font-size: 48px;
-  font-weight: 900;
+  font-weight: 400;
 
-  ${mediaQueries.tablet} {
-    font-size: 36px;
-  }
+  // ${mediaQueries.tablet} {
+  //   font-size: 36px;
+  // }
 
-  ${mediaQueries.largeMobile} {
-    font-size: 24px;
-  }
+  // ${mediaQueries.largeMobile} {
+  //   font-size: 24px;
+  // }
 `;
 
 export const Heading2 = styled.h2`
   margin: 0px;
-  font-family: "Castledown";
+  font-family: "Inter";
   font-size: 36px;
-  font-weight: 900;
+  font-weight: 400;
 
-  ${mediaQueries.tablet} {
-    font-size: 24px;
-  }
+  // ${mediaQueries.tablet} {
+  //   font-size: 24px;
+  // }
 `;
 
 export const Heading3 = styled.h3`
   margin: 0px;
-  font-family: "Castledown";
+  font-family: "Inter";
   font-size: 24px;
-  font-weight: 900;
+  font-weight: 400;
 
-  ${mediaQueries.tablet} {
-    font-size: 20px;
-  }
+  // ${mediaQueries.tablet} {
+  //   font-size: 20px;
+  // }
+`;
+
+export const Heading4 = styled.h4`
+  margin: 0px;
+  font-family: "Inter";
+  font-size: 20px;
+  font-weight: 400;
+
+  // ${mediaQueries.tablet} {
+  //   font-size: 20px;
+  // }
 `;
 
 export const LargeBody = styled.p`
   margin: 0px;
-  font-family: "Satoshi";
+  font-family: "Inter";
   font-size: 20px;
   font-weight: 400;
 
-  ${mediaQueries.tablet} {
-    font-size: 16px;
-  }
+  // ${mediaQueries.tablet} {
+  //   font-size: 16px;
+  // }
 `;
 
 export const LargeBodyMedium = styled.p`
   margin: 0px;
-  font-family: "Satoshi";
+  font-family: "Inter";
   font-size: 20px;
-  font-weight: 500;
+  font-weight: 400;
 
-  ${mediaQueries.tablet} {
-    font-size: 16px;
-  }
+  // ${mediaQueries.tablet} {
+  //   font-size: 16px;
+  // }
 `;
 
 export const LargeBodyBold = styled.p`
   margin: 0px;
-  font-family: "Satoshi";
+  font-family: "Inter";
   font-size: 20px;
   font-weight: 700;
 
-  ${mediaQueries.tablet} {
-    font-size: 16px;
-  }
+  // ${mediaQueries.tablet} {
+  //   font-size: 16px;
+  // }
 `;
 
 export const Body = styled.p`
   padding: 0px;
   margin: 0px;
-  font-family: "Satoshi";
+  font-family: "Inter";
   font-size: 16px;
   font-weight: 400;
 `;
 
 export const BodyBold = styled.p`
   margin: 0px;
-  font-family: "Satoshi";
+  font-family: "Inter";
   font-size: 16px;
   font-weight: 700;
   padding: 0px;
@@ -135,15 +146,23 @@ export const BodyBold = styled.p`
 
 export const BodyHeavy = styled.p`
   margin: 0px;
-  font-family: "Satoshi";
+  font-family: "Inter";
   font-size: 16px;
-  font-weight: 900;
+  font-weight: 400;
 `;
 
 export const Caption = styled.p`
   margin: 0px;
-  font-family: "Satoshi";
-  font-size: 16px;
-  font-weight: 500;
+  font-family: "Inter";
+  font-size: 12px;
+  font-weight: 400;
   font-style: italic;
+`;
+
+export const StyledLink = styled.a`
+  margin: 0px;
+  font-family: "Inter";
+  font-size: 16px;
+  font-weight: 400;
+  text-decoration: none;
 `;
