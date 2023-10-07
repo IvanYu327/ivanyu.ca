8ee8972d747ce52553d39435170daa7b911eb491
Ivan <ivanyu327@gmail.com>
2023-10-06 23:37:28 -0400

    add images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+// Logos
+export { default as HTN2023 } from "./logos/HTN2023.png";
+export { default as WatoLink } from "./logos/WatoLink.png";
+export { default as iVenuto } from "./logos/iVenuto.png";
+export { default as DeepBreathe } from "./logos/DeepBreathe.png";
+export { default as Cognixion } from "./logos/Cognixion.png";
