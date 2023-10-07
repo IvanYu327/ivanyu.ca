8061bffebe743e67ba75a87709a184903709e820
Ivan <ivanyu327@gmail.com>
2023-10-06 23:37:44 -0400

    experience

--- src/copy/experience.ts
+++ src/copy/experience.ts
@@ -1,4 +1,22 @@
-import { Experience } from "../typings/types";
+// import { Experience } from "../typings/types";
+
+import {
+  Cognixion,
+  DeepBreathe,
+  HTN2023,
+  WatoLink,
+  iVenuto
+} from "../assets/images";
+
+type Experience = {
+  position: string;
+  company: string;
+  startYear: number;
+  endYear: number | null;
+  link: string;
+  logo: string;
+  color: string;
+};
 
 export const EXPERIENCES: Experience[] = [
   {
@@ -6,42 +24,54 @@ export const EXPERIENCES: Experience[] = [
     company: "Hack the North",
     startYear: 2023,
     endYear: null,
-    link: ""
+    link: "https://hackthenorth.com/",
+    logo: HTN2023,
+    color: "#333333"
   },
   {
     position: "Software Engineering Intern",
     company: "Deep Breathe",
     startYear: 2023,
     endYear: null,
-    link: ""
+    link: "",
+    logo: DeepBreathe,
+    color: "#333333"
   },
   {
     position: "Software Team Lead",
     company: "WatoLink",
     startYear: 2021,
     endYear: 2023,
-    link: ""
+    link: "",
+    logo: WatoLink,
+    color: "#333333"
   },
   {
     position: "Frontend Developer",
     company: "Hack the North",
     startYear: 2023,
     endYear: 2023,
-    link: ""
+    link: "",
+    logo: HTN2023,
+    color: "#333333"
   },
   {
     position: "Software Engineering Intern",
     company: "iVenuto.com",
     startYear: 2023,
     endYear: 2023,
-    link: ""
+    link: "",
+    logo: iVenuto,
+    color: "#333333"
   },
   {
     position: "Software Developer Intern",
     company: "Cognixion",
     startYear: 2022,
     endYear: 2022,
-    link: ""
+    link: "",
+    logo: Cognixion,
+    color: "#1D2F2F"
   }
 ];
 
--- src/typings/types.ts
+++ src/typings/types.ts
@@ -4,4 +4,5 @@ export type Experience = {
   startYear: number;
   endYear: number | null;
   link: string;
+  logo: File | null;
 };
