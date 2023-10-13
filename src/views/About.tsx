9d0bf838cebf2521b264b91aa13de6a80ff16c70
Ivan <ivanyu327@gmail.com>
2023-10-12 22:39:54 -0400

    about

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -12,6 +12,8 @@ import {
   Heading2,
   Heading3,
   Heading4,
+  Heading5,
+  Heading6,
   LargeBody,
   LargeBodyBold,
   LargeBodyMedium
@@ -63,49 +65,47 @@ const About: React.FC = () => {
     <div>
       <HeroContainer>
         <HeroText>
-          <Heading4>
+          <LargeBody>
             I&apos;m studying Computer Science at the University of Waterloo. A
             software engineer constantly exploring and pushing the bounderies of
             tech.
-          </Heading4>
+          </LargeBody>
           <br />
-          <Heading4>
+          <LargeBody>
             Avid rock climber, marine organism enthusiast, and Tetris addict.
-          </Heading4>
+          </LargeBody>
         </HeroText>
         <Image src={Ivan} />
       </HeroContainer>
-      <Heading4>this website. Last updated Oct 12, 2023</Heading4>
-      <Body>Typography </Body>
+      <LargeBody>this website. </LargeBody>
+      <LargeBody>Last updated Oct 12, 2023</LargeBody>
+      <Heading5>typography </Heading5>
       <Body>
-        Inspired by Akzidenz-Grotesk and the NYC Subway wayfinding system, Söhne
-        is a minimal yet highligh functional and Characterful typeface. Designed
-        by Kris Sowersby and published through Klim Type Foundry. Only one
-        weight (Buch) is used to reduce complexity. Some numbers and color hex
-        codes are displayed in Martian Mono.
+        Everything is in the system default Inter font right now. Currently
+        looking for a fruiter sans serif font.
       </Body>
-      <Body> Technical</Body>
+      <Heading5>technical</Heading5>
       <Body>
-        Built with Next.js, Contentlayer, and Tailwind. Deployed and hosted on
-        Vercel. Cookie-free and open source analytics with Plausible.
+        Built with React, Typescript, and styled-components. Deployed and hosted
+        on Vercel. Source code available on GitHub.
       </Body>
-      <Body>Photography </Body>
+      {/* <Heading4>Photography </Heading4>
       <Body>
         All images were taken or created by Linus Rogge, if not stated
         otherwise. I aim to provide sources for all imagery on this site.
         However, due to the nuances of the internet and platforms for visual
         inspiration, it’s not always possible to do so. Credits on most images
-        can be found by hovering them.{" "}
-      </Body>
-      <Body>Inspiration </Body>
+        can be found by hovering them.
+      </Body> */}
+      <Heading5>inspiration </Heading5>
       <Body>
         Pages that, in no particular order, inspired the current version of this
-        site—visually, substantially, or personally. cristianzennaro.com
+        site — visually, substantially, or personally. cristianzennaro.com
         franvelas.co antonstallboerger.com alexcarpenter.me bmrks.com sdrn.co
         id-c.se carlbarenbrug.com studiolenzing.com christopherdoyle.co
-        klim.co.nz chris-wang.com{" "}
+        klim.co.nz chris-wang.com
       </Body>
-      <Body>Good people </Body>
+      {/* <Body>good people </Body>
       <Body>
         There wouldn’t be enough space to list every important person here, but
         these are a few of the good souls that made a positive impact in my
@@ -113,7 +113,7 @@ const About: React.FC = () => {
         Kindler Henri Bredt Alexander Zank Leander Lenzing Malte Körte Emil Dax
         Florian Kiem Nils Eller Raphael Volpert Valentin Rudloff Sam di Mauro
         Piet Terheyden
-      </Body>
+      </Body> */}
       <CustomMarkdown markdown={markdown} />
       <br />
       <Heading4>Reading</Heading4>
