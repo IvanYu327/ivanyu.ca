5014a792d41652e7d8680a34eaff95436523b198
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2025-02-12 21:57:40 -0800

    sigma

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import vercel from "solid-start-vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), solidPlugin()],
+  plugins: [tailwindcss(), solidPlugin({ adapter: vercel() })],
   server: {
     port: 3434,
   },
