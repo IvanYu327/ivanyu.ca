d4c62de3fa0fc553d48aa8585399c0ba7b94236a
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2025-02-12 23:04:34 -0800

    fade

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { RouteSectionProps } from "@solidjs/router";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+import styles from "./App.module.css";
+
 export default function App({ children }: RouteSectionProps) {
   return (
     <div class="min-h-screen flex flex-col px-5 sm:px-10 min-w-[300px] max-w-[640px] mx-auto">
       <Header />
-      <main class="flex-grow">{children}</main>
+      <main class="flex-grow" classList={{ [styles["fade-in"]]: true }}>
+        {children}
+      </main>
       <Footer />
     </div>
   );
