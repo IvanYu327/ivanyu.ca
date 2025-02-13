3abc0c1a46d09014a5af3d337dae9431972cde7a
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2025-02-13 12:03:13 -0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "solid-js";
+import { createResource, JSX } from "solid-js";
 import IconButton, { XIcon } from "./shared/IconButton";
 import Line from "./shared/Line";
 
@@ -12,7 +12,23 @@ const socialLinks = [
   { icon: FaBrandsInstagram, href: "https://www.instagram.com/ivanyuuu/" },
 ];
 
+async function fetchLastCommit() {
+  const repo = "IvanYu327/ivanyu.ca";
+  const url = `https://api.github.com/repos/${repo}/commits/main`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return new Date(data.commit.committer.date).toLocaleString();
+  } catch (error) {
+    console.error("Error fetching last commit:", error);
+    return "Loading ...";
+  }
+}
+
 export default function Footer() {
+  const [lastUpdate] = createResource(fetchLastCommit);
+
   return (
     <footer class="my-4">
       <Line class="absolute left-0 w-screen" />
@@ -20,7 +36,7 @@ export default function Footer() {
       <div class="flex justify-between items-center mt-4 py-4">
         <div>
           <div class="italic">made by and about ivan yu</div>
-          <div class="italic text-sm">last update: 9:28pm 02/15/2024</div>
+          <div class="italic text-sm">last update: {lastUpdate()}</div>
         </div>
         <div class="flex space-x-4">
           {socialLinks.map(({ icon, href }) => (
